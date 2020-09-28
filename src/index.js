6f5f3526d981ed094bd3b65ae1d43bb84a47f715
MOV22091976 <sas_moskvin@i.ua>
2020-09-28 12:44:42 +0300

    homework10

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import './styles.css';
+import itemsTamplate from './menu-items.hbs';
+
+import menuJson from './menu.json';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const inputRef = document.querySelector('.theme-switch__toggle');
+const localStorageTheme = localStorage.getItem('theme');
+
+inputRef.addEventListener('change', handleInputChange);
+
+function handleInputChange() {
+  if (!inputRef.checked) {
+    addLightTheme();
+  } else {
+    addDarkTheme();
+  }
+}
+
+function addLightTheme() {
+  document.body.classList.remove(Theme.DARK);
+  document.body.classList.add(Theme.LIGHT);
+  localStorage.setItem('theme', Theme.LIGHT);
+}
+
+function addDarkTheme() {
+  document.body.classList.remove(Theme.LIGHT);
+  document.body.classList.add(Theme.DARK);
+  localStorage.setItem('theme', Theme.DARK);
+}
+
+function lookLocalStorage() {
+  if (localStorageTheme === Theme.DARK) {
+    document.body.classList.toggle(Theme.DARK);
+    localStorage.clear();
+  } else {
+    document.body.classList.toggle(Theme.LIGHT);
+    localStorage.clear();
+  }
+}
+
+lookLocalStorage();
+
+const markup = itemsTamplate(menuJson);
+
+const galleryRef = document.querySelector('.js-menu');
+galleryRef.insertAdjacentHTML('beforeend', markup);
