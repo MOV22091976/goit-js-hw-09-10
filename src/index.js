c85a56975955759fc05ae32eb38f43dc798e558c
MOV22091976 <sas_moskvin@i.ua>
2020-09-29 22:01:21 +0300

    homework10

--- src/index.js
+++ src/index.js
@@ -37,9 +37,11 @@ function lookLocalStorage() {
   if (localStorageTheme === Theme.DARK) {
     document.body.classList.toggle(Theme.DARK);
     localStorage.clear();
+    inputRef.checked = true;
   } else {
     document.body.classList.toggle(Theme.LIGHT);
     localStorage.clear();
+    inputRef.checked = false;
   }
 }
 
